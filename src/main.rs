5ac499ad6c236699ba36c8552eb0aa9b89658743
grappell <graysonpa@gmail.com>
2025-04-14 15:15:49 -0700

    Start on raytracing in weekend, on listing 9

---
+++ src/main.rs
@@ -0,0 +1,91 @@
+mod util;
+
+use util::{Ray, Vec3};
+
+use Vec3 as Point;
+use Vec3 as Color;
+
+// NOTES --------------------------
+// y-axis go up, the x-axis to the right, and the negative z-axis pointing in the viewing direction
+
+fn main() {
+
+    println!(" \n Starting Code \n ");
+
+    let v1 = Point::new(1.0, 2.0, 3.0);
+    println!("v1: {:?}", v1);
+
+    // System Constants
+    const IMG_PATH: &str = "res/image.png";
+
+    // Image Constants
+    const ASPECT_RATIO: f32 = 16.0 / 9.0; // Ideal aspect ratio
+    const IMG_WIDTH: u32 = 256;
+    const IMG_HEIGHT: u32 = (IMG_WIDTH as f32 / ASPECT_RATIO).min(1.0) as u32;
+
+    // Viewport Constants
+    const VIEWPORT_HEIGHT: f32 = 2.0;
+    const VIEWPORT_WIDTH: f32 = VIEWPORT_HEIGHT * (IMG_WIDTH / IMG_HEIGHT) as f32;
+
+    // Camera Constants
+    const FOCAL_LENGTH: f32 = 1.0;
+    let camera_center: Point = Point::new(0.0, 0.0, 0.0);
+
+    // Vectors across the horizontal and down the vertical viewport edges
+    let viewport_u = Vec3::new(VIEWPORT_WIDTH, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -VIEWPORT_HEIGHT, 0.0);
+
+    // Horizontal and vertical delta vectors from pixel to pixel
+    let pixel_delta_u = viewport_u / IMG_WIDTH as f32;
+    let pixel_delta_v = viewport_v / IMG_HEIGHT as f32;
+
+    // Location of upper left pixel
+    let viewport_top_left = camera_center - Vec3::new(0.0, 0.0, FOCAL_LENGTH) - viewport_u / 2.0 - viewport_v / 2.0;
+    let first_pixel_loc = viewport_top_left + (( pixel_delta_u / 2.0 + pixel_delta_v / 2.0) * 0.5 as f32);
+
+
+    println!(" \n Running Raytrace... \n ");
+
+
+    let mut img_buf: image::ImageBuffer<image::Rgb<u8>, Vec<u8>> = image::ImageBuffer::new(IMG_WIDTH, IMG_HEIGHT);
+    let mut prev_line = 0;
+
+    // Iterate over the coordinates and pixels of the image
+    for (x, y, pixel) in img_buf.enumerate_pixels_mut() {
+
+        if y != prev_line {
+            println!("Rendering line {} of {} ({}%)", y, IMG_HEIGHT, y * 100 / IMG_HEIGHT);
+            prev_line = y;
+        }
+
+        let c = Color::new(
+            x as f32 / IMG_WIDTH as f32,
+            y as f32 / IMG_HEIGHT as f32,
+            1.0 - ((y + x) as f32 / (IMG_WIDTH + IMG_HEIGHT) as f32),
+        );
+
+        write_color(pixel, c);
+
+    }
+
+    println!("\n --- Done rendering --- \n");
+    img_buf.save(IMG_PATH).unwrap();
+    println!("Image saved to {}", IMG_PATH);
+    println!("Raytrace finished.");
+
+}
+
+fn ray_color(ray: &Ray) -> Color {
+    Color::new(0.0, 0.0, 0.0)
+}
+
+fn write_color(
+    pixel: &mut image::Rgb<u8>,
+    color: Color,
+) {
+    let r = (color.x * 255.0) as u8;
+    let g = (color.y * 255.0) as u8;
+    let b = (color.z * 255.0) as u8;
+
+    *pixel = image::Rgb([r, g, b]);
+}
---
+++ src/util.rs
@@ -0,0 +1,115 @@
+use std::ops;
+
+#[derive(Debug, Clone, Copy)]
+pub struct Vec3 {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Vec3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
+        Vec3 { x, y, z }
+    }
+
+    pub fn normalize(&self) -> Vec3 {
+        let len = self.length();
+        Vec3::new(self.x / len, self.y / len, self.z / len)
+    }
+
+    pub fn length_squared(&self) -> f32 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn length(&self) -> f32 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn dot(u: &Vec3, v: &Vec3) -> f32 {
+        u.x * v.x + u.y * v.y + u.z * v.z
+    }
+
+    pub fn cross(u: &Vec3, v: &Vec3) -> Vec3 {
+        Vec3::new(
+            u.y * v.z - u.z * v.y,
+            u.z * v.x - u.x * v.z,
+            u.x * v.y - u.y * v.x,
+        )
+    }
+}
+
+impl ops::Add<&Vec3> for &Vec3 {
+    type Output = Vec3;
+    fn add(self, other: &Vec3) -> Vec3 {
+        Vec3::new(self.x + other.x, self.y + other.y, self.z + other.z)
+    }
+}
+
+impl ops::Add<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn add(self, other: Vec3) -> Vec3 {
+        Vec3::new(self.x + other.x, self.y + other.y, self.z + other.z)
+    }
+}
+
+impl ops::Sub<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn sub(self, other: Vec3) -> Vec3 {
+        Vec3::new(self.x - other.x, self.y - other.y, self.z - other.z)
+    }
+}
+
+impl ops::Mul<&Vec3> for &Vec3 {
+    type Output = Vec3;
+    fn mul(self, other: &Vec3) -> Vec3 {
+        Vec3::new(self.x * other.x, self.y * other.y, self.z * other.z)
+    }
+}
+
+impl ops::Mul<f32> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, value: f32) -> Vec3 {
+        Vec3::new(self.x * value, self.y * value, self.z * value)
+    }
+}
+
+impl ops::Div<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn div(self, other: Vec3) -> Vec3 {
+        Vec3::new(self.x / other.x, self.y / other.y, self.z / other.z)
+    }
+}
+
+impl ops::Div<f32> for Vec3 {
+    type Output = Vec3;
+    fn div(self, value: f32) -> Vec3 {
+        self * (1.0 / value)
+    }
+}
+
+// ----------------------------------------------
+// Ray definition -------------------------------
+// ----------------------------------------------
+#[derive(Debug, Clone, Copy)]
+pub struct Ray {
+    pub origin: Vec3,
+    pub direction: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Vec3, direction: Vec3) -> Ray {
+        Ray { origin, direction }
+    }
+
+    pub fn at(&self, t: f32) -> Vec3 {
+        self.origin + (self.direction * t)
+    }
+
+    pub fn direction(&self) -> &Vec3 {
+        &self.direction
+    }
+
+    pub fn origin(&self) -> &Vec3 {
+        &self.origin
+    }
+}
